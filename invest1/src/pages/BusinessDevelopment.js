10f4348936a3a0bc6d3df1ab3dfe5570147d0843
Aleksey <a.babykiny@gmail.com>
2023-11-29 12:32:07 +0200

    add pdf

--- invest1/src/pages/BusinessDevelopment.js
+++ invest1/src/pages/BusinessDevelopment.js
@@ -1,13 +1,27 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 
 const BusinessDevelopment = () => {
   const { name } = useParams();
+
+  const docs = [
+    {
+      uri: require(`../const/${name}.pdf`),
+      fileType: "pdf",
+      fileName: `${name}`,
+    },
+  ];
+
   return (
     <Container>
       <div>BusinessDevelopment</div>
       <div>{name}</div>
+      <DocViewer
+        style={{ height: 1000 }}
+        documents={docs}
+        pluginRenderers={DocViewerRenderers}
+      />
     </Container>
   );
 };
